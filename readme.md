96fef22a3c2f83c0c74315da5098a9911fd5c139
J/A <archer884@gmail.com>
2015-03-23 23:01:03 -0400

    Hello world!
    Server is operational (in that it can receive and respond to requests).
    Of course, the only request route it has so far is / (root) and the only
    response it gives at the moment is "Hello," but this is a good start.
    
    Up next: Twilio integration!

---
+++ readme.md
@@ -0,0 +1,10 @@
+leaded
+------
+
+Web-based fuel expenditure tracking system.
+
+Features:
+- Mobile integration (via Twilio)
+    - User texts `<gallons> <cost>` and the server stores this information in their account.
+    - Responsive site design that looks at least kinda native on both android and ios.
+    - Full native on Windows Phone (if I happened to have one).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![feature(core)]
+
+extern crate iron;
+extern crate router;
+
+use iron::{ Iron, IronResult, Request, Response };
+use iron::status;
+use router::Router;
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+    let router = build_router();
+
+    match Iron::new(router).http("localhost:1337") {
+        Ok(_) => println!("LEADED server started."),
+        Err(e) => println!("{}", Error::description(&e)),
+    }
+}
+
+fn handler(_: &mut Request) -> IronResult<Response> {
+    Ok(Response::with((status::Ok, "Hello!")))
+}
+
+fn build_router() -> Router {
+    let mut router = Router::new();
+    router.get("/", handler);
+    router
 }
